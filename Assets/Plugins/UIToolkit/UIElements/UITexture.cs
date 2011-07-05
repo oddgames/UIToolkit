fa220c673eb14df677c59e45ef6e08e9ff1e4e37
reissgrant <argrant1977@gmail.com>
2011-07-05 15:59:11 -0400

    New Texture Class for displaying a texture with no functionality.
    Signed-off-by: reissgrant <argrant1977@gmail.com>

--- Assets/Plugins/UIToolkit/UIElements/UITexture.cs
+++ Assets/Plugins/UIToolkit/UIElements/UITexture.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class UITexture : UISprite
 {
-	private UIUVRect _normalUVframe; // Holds a copy of the uvFrame that the button was initialized with
+	private UIUVRect _normalUVframe;
 
 	#region Constructors/Destructor
 	
@@ -18,18 +18,16 @@ public class UITexture : UISprite
 	}
 	
 	public static UITexture create( UIToolkit manager, string filename, int xPos, int yPos, int depth )
-	{
-		// grab the texture details for the normal state
+	{		
 		var normalTI = manager.textureInfoForFilename( filename );
 		var frame = new Rect( xPos, yPos, normalTI.frame.width, normalTI.frame.height );
 		
-		// create the button
+	
 		return new UITexture( manager, frame, depth, normalTI.uvRect);
 	}
 
 	public UITexture( UIToolkit manager, Rect frame, int depth, UIUVRect uvFrame):base( frame, depth, uvFrame )
 	{
-		// Save a copy of our uvFrame here so that when highlighting turns off we have the original UVs
 		_normalUVframe = uvFrame;
 		
 		manager.addSprite( this );

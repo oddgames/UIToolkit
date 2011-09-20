07605f86468967653d126dbc6ec10cfe059160b1
Mike <desaro@gmail.com>
2011-09-19 18:19:05 -0700

    reformatted file

--- Assets/Plugins/UIToolkit/Containers/UIBackgroundLayout.cs
+++ Assets/Plugins/UIToolkit/Containers/UIBackgroundLayout.cs
@@ -5,12 +5,11 @@ using System.Collections.Generic;
 
 public class UIBackgroundLayout : UIAbstractContainer
 {	
-	public UIBackgroundLayout( string filename ) : this(UI.firstToolkit, filename) {}
+	public UIBackgroundLayout( string filename ) : this( UI.firstToolkit, filename ) {}
 
 	public UIBackgroundLayout( UIToolkit manager, string filename ) : base( UILayoutType.BackgroundLayout ) 
 	{
-		int depth = 2;
-		var background = manager.addSprite( filename, 0, 0, depth);
+		var background = manager.addSprite( filename, 0, 0, 2 );
 		addChild( background );
 		
 		//set dimensions of container based on background texture dimensions
